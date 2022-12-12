dcdd750653ce9068824c067f840f6c2925c91993
Виктор Шилигин <Victor918@mail.ru>
2022-12-12 14:10:38 +0300

    API

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,30 @@
+from posts.models import Comment, Group, Post
+from rest_framework import serializers
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'author', 'group', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('author', 'post')
