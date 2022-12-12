d984d447354ca39f97100aacb352caec50173709
Виктор Шилигин <Victor918@mail.ru>
2022-12-12 18:26:50 +0300

    V_2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,10 +7,10 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 router = routers.DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
-router.register(r'posts/(?P<post_id>\d+)/comments',
+router.register('posts/(?P<post_id>\\d+)/comments',
                 CommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('api/v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls)),
 ]
